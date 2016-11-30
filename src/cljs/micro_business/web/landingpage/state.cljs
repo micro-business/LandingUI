9527845a0dcf09e603516a5e0f8dcf267f11df9e
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-11-30 20:37:45 +1300

    Refactored state file

--- src/cljs/micro_business/web/landingpage/state.cljs
+++ src/cljs/micro_business/web/landingpage/state.cljs
@@ -1,16 +1,20 @@
 (ns micro-business.web.landingpage.state)
 
+(def ^:private navigationBar {:id "topNavigationBar"
+                              :brand "Micro Business"
+                              :rightToLeftAlignment false})
+
+(def ^:private signedInNavigationItems {:navigationItems
+                                        [{:id "signout" :caption "Sign out" :onClickedQueryExpression `[('signout/clicked) :root-view]}]})
+
+(def ^:private signedOutNavigationItems {:navigationItems
+                                         [{:id "signin" :caption "Sign in" :onClickedQueryExpression `[('signin/clicked) :root-view]}]})
+
 (def state
   {:signedInLandingPage
    {:id :signedInLandingPage
-    :navigationBars [{:id "topNavigationBar"
-                      :brand "Micro Business"
-                      :rightToLeftAlignment false
-                      :navigationItems [{:id "signout" :caption "Sign out" :onClickedQueryExpression `[('signout/clicked) :root-view]}]}]}
+    :navigationBars [(merge navigationBar signedInNavigationItems)]}
 
    :signedOutLandingPage
    {:id :signedOutLandingPage
-    :navigationBars [{:id "topNavigationBar"
-                      :brand "Micro Business"
-                      :rightToLeftAlignment false
-                      :navigationItems [{:id "signin" :caption "Sign in" :onClickedQueryExpression `[('signin/clicked) :root-view]}]}]}})
+    :navigationBars [(merge navigationBar signedOutNavigationItems)]}})
