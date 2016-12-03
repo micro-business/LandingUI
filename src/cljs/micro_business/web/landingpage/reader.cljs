68c2bebf5909cd7b6c959338af5823c528a5d9bd
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-12-04 01:41:26 +1300

    Refactored

--- src/cljs/micro_business/web/landingpage/reader.cljs
+++ src/cljs/micro_business/web/landingpage/reader.cljs
@@ -7,11 +7,7 @@
         landingPages (map #(get-in state [:landing-page/by-id %]) landingPageIDs)
         navItems (state :navItem/by-id)
         navBars (state :navBar/by-id)
-        updatedNavBars (map (fn [navBar]
-                              (let [detailPart (last navBar)
-                                    navItemDetails (map #(navItems (last %)) (detailPart :navigationItems))
-                                    newNavBar (assoc-in detailPart [:navigationItems] (into [] navItemDetails))]
-                                newNavBar)) navBars)
+        updatedNavBars (navbarreader/getNavigationBars state)
         updatedLandingPages (map (fn [landingPage]
                                    (let [navigationBarIDs (map #(last %) (landingPage :navigationBars))
                                          navigationBarsInDetail (map (fn [navigationBarID]
